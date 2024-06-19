6039f1f11529f8295b04f310fe9f8480fbfa14b2
Aakash <0412.aakash@gmail.com>
2024-06-19 16:30:17 +0530

    Code Clean Up

--- src/main/java/com/itime/compoff/exception/ValidationException.java
+++ src/main/java/com/itime/compoff/exception/ValidationException.java
@@ -1,8 +1,11 @@
 package com.itime.compoff.exception;
 
 
+import java.io.Serial;
+
 public class ValidationException extends CommonException{
 
+	@Serial
 	private static final long serialVersionUID = -3949136580323575600L;
 
 	public ValidationException(String message) {
